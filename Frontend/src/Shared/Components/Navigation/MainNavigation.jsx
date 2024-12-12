7e4b252191cbf258895ca5a44caf3fa3e35f9042
Kartheeek Muppuri <kartheek.muppiri@nowitservices.com>
2024-12-12 21:27:13 +0530

    commit on 12 12

--- Frontend/src/Shared/Components/Navigation/MainNavigation.jsx
+++ Frontend/src/Shared/Components/Navigation/MainNavigation.jsx
@@ -1,35 +1,47 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-import './MainNavigation.css'
-import MainHeader from './MainHeader';
-import NavLinks from './NavLinks';
-import SideDrawer from './SideDrawer';
+import "./MainNavigation.css";
+import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/BackDrop";
 
-const MainNavigation = props => {
+const MainNavigation = (props) => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawerHadler = () => {
+        setDrawerIsOpen(true);
+    };
+
+    const closeDrawerHandler = () => {
+        setDrawerIsOpen(false);
+    };
     return (
         <>
-            <SideDrawer>
-                <nav className='main-navigation__drawer-nav'>
+            {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+            <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+                <nav className="main-navigation__drawer-nav">
                     <NavLinks />
                 </nav>
             </SideDrawer>
+
             <MainHeader>
-                <button className="main-navigation__menu-btn">
+                <button className="main-navigation__menu-btn" onClick={openDrawerHadler}>
                     <span />
                     <span />
                     <span />
                 </button>
 
-
-                <h1 className='main-navigation__title'>
-                    <Link to='/'> YourPlaces</Link>
+                <h1 className="main-navigation__title">
+                    <Link to="/"> YourPlaces</Link>
                 </h1>
-                <nav className='main-navigation__header-nav'>
+                <nav className="main-navigation__header-nav">
                     <NavLinks />
                 </nav>
             </MainHeader>
         </>
-    )
+    );
 };
 
 export default MainNavigation;
