487890bbf0637e542db07da05bfe3be967daaa0e
SivakumarErugu <sivakumar.erugu@nowitservices.com>
2025-01-02 21:59:47 +0530

    node code split

--- Backend/app.js
+++ Backend/app.js
@@ -1,26 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const placesRoutes = require('./routes/places-routes');
 
 const app = express();
 
-app.use((req,res,next) => {
-    let body = '';
-    req.on('end', () => {
-        const userName = body.split('=')[1];
-        if(userName) {
-            req.body = {name:userName};
-        }
-        next();
-    });
-    req.on('data', chunk => {
-        body+= chunk;
-    })
-});
+app.use(placesRoutes);
 
-app.use((req,res,next) => {
-    if(req.body) {
-        return res.send('<h1>'+ req.body.name +'</h1>')
-    }
-    res.send('<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>')
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.listen(5000); 
+
+
+app.listen(5000);  
