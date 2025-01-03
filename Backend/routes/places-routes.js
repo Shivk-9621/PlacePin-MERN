f19c096b3980dd3f15b199ddfbe59e778cf89fe4
SivakumarErugu <sivakumar.erugu@nowitservices.com>
2025-01-03 21:01:09 +0530

    paths

--- Backend/routes/places-routes.js
+++ Backend/routes/places-routes.js
@@ -2,9 +2,37 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    console.log('GET Request in places');
-    res.json({ message: 'its working' });
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the most famous sky scrapers in the world',
+        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
+        address: '20 W 34th St, New York, NY 10118, United States',
+        location: {
+            lat: 40.748817,
+            lng: -73.985428
+        },
+        creator: 'u1'
+    }
+]
+
+router.get('/:pid', (req, res, next) => {
+    const placeId = req.params.pid;
+    const place = DUMMY_PLACES.find(p => {
+        return p.id === placeId;
+    })
+    res.json({ place });
 });
+
+router.get('/user/:uid', (req,res,next) => {
+    const userId = req.params.uid;
+
+    const place = DUMMY_PLACES.find(p => {
+        return p.creator === userId;
+    })
+
+    res.json({place});
+})
 
 module.exports = router;
